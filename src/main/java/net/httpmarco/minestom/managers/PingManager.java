1445041ed9874d243068e242f7df7ea49baee3d3
Aaron <bauhd@gmx.net>
2022-11-12 01:31:43 +0100

    improved PingManager

--- src/main/java/net/httpmarco/minestom/managers/PingManager.java
+++ src/main/java/net/httpmarco/minestom/managers/PingManager.java
@@ -1,48 +1,45 @@
 package net.httpmarco.minestom.managers;
 
 import net.httpmarco.minestom.Minestom;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.server.ServerListPingEvent;
-import net.minestom.server.network.ConnectionManager;
 import net.minestom.server.ping.ResponseData;
 
 import java.io.File;
-import java.io.InputStream;
 import java.util.Base64;
 
 public class PingManager {
 
-    public PingManager(){
-        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
-        ConnectionManager connectionManager = MinecraftServer.getConnectionManager();
+    public PingManager() {
+        final var globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        final var property = Minestom.getProperty();
+        final var motd = LegacyComponentSerializer.legacySection().deserialize(property.getMotd());
 
+        String icon = null;
+        try (final var stream = new File(property.getIcon()).toURI().toURL().openStream()) {
+            if (stream != null) {
+                icon = "data:image/png;base64," + Base64.getEncoder().encodeToString(stream.readAllBytes());
+            }
+        } catch (Exception ignored) {
+            // favicon.png not found
+        }
+
+        final var finalIcon = icon;
         globalEventHandler.addListener(ServerListPingEvent.class, serverListPingEvent -> {
-            ResponseData responseData = new ResponseData();
-            responseData.setDescription(Minestom.getProperty().getMotd());
-            responseData.setMaxPlayer(Minestom.getProperty().getMaxPlayers());
-            if (Minestom.getProperty().getOnlinePlayers() == -1){
-                // if set to -1 its autofilled with current count of players on the server
-                responseData.setOnline(connectionManager.getOnlinePlayers().size());
-            } else {
+            final var responseData = new ResponseData();
+            responseData.setDescription(motd);
+            responseData.setMaxPlayer(property.getMaxPlayers());
+            if (property.getOnlinePlayers() != -1) {
                 responseData.setOnline(Minestom.getProperty().getOnlinePlayers());
             }
 
-            String icon = null;
-            try (InputStream stream = new File(Minestom.getProperty().getIcon()).toURL().openStream()) {
-                if (stream != null) {
-                    icon = "data:image/png;base64," + Base64.getEncoder().encodeToString(stream.readAllBytes());
-                }
-            } catch (Exception ignored) {
-                // favicon.png not found
-            } finally {
-                if (icon != null){
-                    responseData.setFavicon(icon);
-                }
+            if (finalIcon != null) {
+                responseData.setFavicon(finalIcon);
             }
 
             serverListPingEvent.setResponseData(responseData);
-        } );
+        });
     }
 
 }
