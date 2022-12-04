521ad8cc57866e09dc3e97d8451a701ca5563577
Mirco Lindenau <mirco.lindenau@gmx.de>
2022-12-04 14:31:58 +0100

    Implement instance provider

---
+++ src/main/java/net/http/mineservice/instances/generator/VoidGenerator.java
@@ -0,0 +1,12 @@
+package net.http.mineservice.instances.generator;
+
+import net.minestom.server.instance.generator.GenerationUnit;
+import net.minestom.server.instance.generator.Generator;
+import org.jetbrains.annotations.NotNull;
+
+public final class VoidGenerator implements Generator {
+
+    @Override
+    public void generate(@NotNull GenerationUnit unit) {}
+
+}
